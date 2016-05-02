16b24b66b3e2f88136e6ca8608429bb2f0e4eb99
DSobscure <snbruce13@yahoo.com.tw>
2016-05-02 22:26:10 +0800

    Initial

---
+++ CS2048/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2048
+{
+    public enum Direction : int
+    {
+        No,
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
---
+++ CS2048/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2048
+{
+    public class Game
+    {
+        public Board Board { get; private set; }
+        public int Score { get; private set; }
+        public bool IsEnd
+        {
+            get
+            {
+                bool result = false;
+                for(int i = (int)Direction.Up; i < (int)Direction.Right; i++)
+                {
+                    if()
+                }
+            }
+        }
+        public bool MoveCheck(Direction direction)
+        {
+
+        }
+        public bool Move(Direction direction, out double reward)
+        {
+            reward = 0;
+            return false;
+        }
+    }
+}
